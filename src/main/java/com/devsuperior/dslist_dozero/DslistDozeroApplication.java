a57646c2933f264621f83b8e3af1cbc372422d2c
gabriel <gabriel--fiss@hotmail.com>
2024-11-11 19:51:24 -0300

    projeto criado

---
+++ src/main/java/com/devsuperior/dslist_dozero/DslistDozeroApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslist_dozero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistDozeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistDozeroApplication.class, args);
+	}
+
+}
